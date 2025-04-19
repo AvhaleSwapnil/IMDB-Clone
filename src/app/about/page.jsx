5cd9928704948f47409a5a52aa27abe996ce30a3
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-04-19 10:57:25 +0530

    add about section

---
+++ src/app/about/page.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl text-amber-600 font-medium">About</h1>
+      <p>
+        Welcome to our movie discovery platform! We are a team of movie lovers
+        who believe in the magic of storytelling through cinema. Our mission is
+        to bring movie fans together under one roof, offering an immersive
+        experience to explore films, stay updated with industry trends, and find
+        recommendations tailored to your tastes.
+      </p>
+      <p>
+        Our platform hosts an extensive collection of movies from around the
+        world, ranging from timeless classics to the latest blockbusters. You
+        can effortlessly search by title, actor, genre, or release date. Each
+        movie listing includes detailed descriptions, trailers, ratings, and
+        user reviews — helping you make informed choices for your next movie
+        night.
+      </p>
+      <p>
+        Beyond being a movie database, we aim to build a vibrant community of
+        cinema enthusiasts. Participate in discussions, share your thoughts on
+        recent films, and discover hidden gems through recommendations from
+        fellow users. Whether you're here to explore, review, or simply browse,
+        we hope our platform becomes your go-to place for all things movies.
+      </p>
+    </div>
+  );
+}
--- src/app/compnents/DarkModeSwitch.jsx
+++ src/app/compnents/DarkModeSwitch.jsx
@@ -1,17 +1,10 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 
 export default function DarkModeSwitch() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme, systemTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
 
   const currentTheme = theme === "system" ? systemTheme : theme;
 
