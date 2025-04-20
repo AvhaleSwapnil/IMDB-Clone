540371bf2ee89a56e2a12c6b137b414c0862c48e
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-04-20 16:26:15 +0530

    fetch data from tmdb api to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,29 @@
-import React from "react";
+const API_KEY = process.env.API_KEY;
 
-export default function page() {
-  return <div>Home</div>;
+import React from "react";
+import { Results } from "./compnents";
+
+export default async function Page({ searchParams }) {
+  const genre = searchParams?.genre || "fetchTrending";
+
+  const endpoint =
+    genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3${endpoint}?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 60 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
