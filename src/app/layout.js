283fae85028c56438d166eb25e6219042ec95d20
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-04-25 15:44:42 +0530

    add loading effect using next js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "./Providers";
 import { Header, Navbar } from "./compnents";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
         <Providers>
           <Header />
           <Navbar />
+          {/* <SearchBox /> */}
           {children}
         </Providers>
       </body>
